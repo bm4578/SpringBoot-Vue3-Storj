5aff5dd3a60ecf0b7b48fa5779779695eb60ad5a
bm4578 <bmt4578@gmail.com>
2023-02-15 21:45:14 +0800

    完成基础业务逻辑

--- SpringBoot/src/main/java/com/by/controller/FileController.java
+++ SpringBoot/src/main/java/com/by/controller/FileController.java
@@ -5,7 +5,6 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,22 +14,19 @@ import org.springframework.web.bind.annotation.RestController;
  * @title
  * @date 2023/2/15 1:57 下午
  */
-@Api(value = "/api", tags = {""})
+@Api(value = "/api", tags = {"转换接口"})
 @RestController
 @RequestMapping("/api")
 public class FileController {
-    @ApiOperation(value = "测试", notes = "", httpMethod = "GET")
-    @GetMapping
-    public String test(){
-        return "hello";
-    }
-
     @ApiImplicitParams({
-            @ApiImplicitParam(paramType = "query", dataType = "string", name = "url", value = "", required = true)
+            @ApiImplicitParam(paramType = "query", dataType = "string", name = "url", value = "", required = true),
+            @ApiImplicitParam(paramType = "query", dataType = "string", name = "newName", value = "", required = true)
     })
     @ApiOperation(value = "传入接口", notes = "", httpMethod = "POST")
     @PostMapping
-    public ResultModel URL(String url){
-        return ResultModel.ok(url);
+    public ResultModel URL(String url ,String newName){
+
+        String newURL = url+"?download=1 -O" + newName;
+        return ResultModel.ok(newURL);
     }
 }
